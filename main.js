6ddac7818b91a381f7597beaa30804eea1e25ba1
mengqian02 <mengqian02@gmail.com>
2016-11-18 10:54:15 +0800

    try diff chunks

---
+++ main.js
@@ -0,0 +1,6 @@
+var style = require("./less/index.less")
+var styleKangaroo = require("./kangaroo/index.less")
+// style.use()
+
+let name = "mactive";
+console.log("this is js",name);
--- webpack.config.js
+++ webpack.config.js
@@ -2,25 +2,44 @@ var autoprefixer = require('autoprefixer');
 var cssnano = require('cssnano');
 var webpack = require('webpack');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var path = require('path')
+
+let extractCSS = new ExtractTextPlugin('stylesheets/[name].css');
+let extractLESS = new ExtractTextPlugin('less/[name].css');
+let extractLESSKangaroo = new ExtractTextPlugin('kangaroo/[name].css');
 
 module.exports = {
   context: __dirname,
-  entry: "./index.less",
+  entry: "./main.js",
   output: {
-      path: __dirname,
+      path: __dirname+ '/build',
       filename: "bundle.js"
   },
   module: {
-      loaders:[{
-          test: /\.less$/,
-          loader: ExtractTextPlugin.extract("style","css!postcss!less")
-      }]
-    },
-    plugins:[
-      new ExtractTextPlugin("bundle.css")
-    ],
-    postcss: [ 
-      autoprefixer({ browsers: ['last 2 versions'] }), 
-      cssnano() 
+    preloader:[],
+    loaders:[
+      {
+        test: /\.less$/,
+        include: [
+          path.resolve(__dirname, "less/"),
+        ],
+        loader: extractLESS.extract("style","css!postcss!less")
+      },
+      {
+        test: /\.less$/,
+        include: [
+          path.resolve(__dirname, "kangaroo/"),
+        ],
+        loader: extractLESSKangaroo.extract("style","css!postcss!less")
+      },
     ]
+  },
+  plugins:[
+      extractLESS,
+      extractLESSKangaroo
+  ],
+  postcss: [ 
+    autoprefixer({ browsers: ['last 3 versions'] }), 
+    // cssnano() 
+  ]
 };
